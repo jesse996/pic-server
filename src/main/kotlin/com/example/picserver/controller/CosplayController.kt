b184bc0d2f08fe43e560c41c69032bbb3d48f0ea
Jesse <szx961102@gmail.com>
2021-08-07 00:53:48 +0800

    update pic page

--- src/main/kotlin/com/example/picserver/controller/CosplayController.kt
+++ src/main/kotlin/com/example/picserver/controller/CosplayController.kt
@@ -1,7 +1,9 @@
 package com.example.picserver.controller
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page
 import com.example.picserver.common.CommonResult
 import com.example.picserver.entity.Pic
+import com.example.picserver.entity.vo.PageReq
 import com.example.picserver.service.PicService
 import org.springframework.web.bind.annotation.*
 import javax.annotation.Resource
@@ -9,23 +11,18 @@ import javax.annotation.Resource
 @RestController
 @RequestMapping("cosplay")
 class CosplayController(@Resource private val picService: PicService) {
-    @GetMapping("")
-    fun list(): CommonResult<Any> =
-        CommonResult.success(picService.list())
+    @PostMapping("")
+    fun list(@RequestBody pageReq: PageReq) = CommonResult.success(picService.page(Page(pageReq.current, pageReq.size)))
 
     @GetMapping("/:id")
-    fun getById(@RequestParam("id") id: Long): CommonResult<Pic> =
-        CommonResult.success(picService.getById(id))
+    fun getById(@RequestParam("id") id: Long) = CommonResult.success(picService.getById(id))
 
     @PostMapping("/")
-    fun add(@RequestBody pic: Pic): CommonResult<Boolean> =
-        CommonResult.success(picService.save(pic))
+    fun add(@RequestBody pic: Pic) = CommonResult.success(picService.save(pic))
 
     @DeleteMapping("/:id")
-    fun delById(@RequestParam("id") id: Long): CommonResult<Boolean> =
-        CommonResult.success(picService.removeById(id))
+    fun delById(@RequestParam("id") id: Long) = CommonResult.success(picService.removeById(id))
 
     @PutMapping("/:id")
-    fun updateById(@RequestBody pic: Pic): CommonResult<Boolean> =
-        CommonResult.success(picService.updateById(pic))
+    fun updateById(@RequestBody pic: Pic) = CommonResult.success(picService.updateById(pic))
 }
---
+++ src/main/kotlin/com/example/picserver/entity/vo/PageReq.kt
@@ -0,0 +1,4 @@
+package com.example.picserver.entity.vo
+
+data class PageReq(val current:Long,val size:Long) {
+}
