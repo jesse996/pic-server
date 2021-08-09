c651c4e301fea68f28f70f1dd54491baf106d46b
Jesse <szx961102@gmail.com>
2021-08-09 23:48:41 +0800

    轮播图数量限制

--- src/main/kotlin/com/example/picserver/controller/CarouselController.kt
+++ src/main/kotlin/com/example/picserver/controller/CarouselController.kt
@@ -4,6 +4,7 @@ import com.example.picserver.common.CommonResult
 import com.example.picserver.entity.Carousel
 import com.example.picserver.service.CarouselService
 import io.swagger.annotations.ApiOperation
+import org.apache.ibatis.ognl.DynamicSubscript.last
 import org.springframework.web.bind.annotation.*
 import javax.annotation.Resource
 
@@ -12,7 +13,8 @@ import javax.annotation.Resource
 class CarouselController(@Resource private val carouselService: CarouselService) {
     @ApiOperation("获取轮播图")
     @GetMapping("")
-    fun list() = CommonResult.success(carouselService.list())
+    fun list(@RequestParam(value = "count") count: Int?) =
+        CommonResult.success(carouselService.ktQuery().last("limit ${count?:4}").list())
 
     @GetMapping("/{id}")
     fun getById(@PathVariable("id") id: Long): CommonResult<Carousel> =
