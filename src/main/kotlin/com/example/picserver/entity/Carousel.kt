c53c55ad177e0a4bc804f2b0dbaf5d853239b361
Jesse <szx961102@gmail.com>
2021-08-09 23:42:37 +0800

    carousel add href and title

--- src/main/kotlin/com/example/picserver/entity/Carousel.kt
+++ src/main/kotlin/com/example/picserver/entity/Carousel.kt
@@ -14,7 +14,7 @@ import io.swagger.annotations.ApiModelProperty;
  * </p>
  *
  * @author jesse
- * @since 2021-08-06
+ * @since 2021-08-09
  */
 @ApiModel(value="Carousel对象", description="轮播图表")
 open class Carousel : Serializable {
@@ -28,6 +28,10 @@ open class Carousel : Serializable {
     @TableField(fill = FieldFill.INSERT_UPDATE)
     var updateTime: LocalDateTime? = null
     var deleted: Int? = null
+    @ApiModelProperty(value = "链接")
+    var href: String? = null
+    @ApiModelProperty(value = "标题")
+    var title: String? = null
 
 
     override fun toString(): String {
@@ -37,6 +41,8 @@ open class Carousel : Serializable {
         ", createTime=" + createTime +
         ", updateTime=" + updateTime +
         ", deleted=" + deleted +
+        ", href=" + href +
+        ", title=" + title +
         "}"
     }
 }
---
+++ src/main/resources/db/migration/V1_2__carousel_add_url.sql
@@ -0,0 +1,4 @@
+ALTER TABLE carousel
+    ADD COLUMN href VARCHAR(300) COMMENT '链接';
+ALTER TABLE carousel
+    ADD COLUMN title VARCHAR(50) COMMENT '标题';
