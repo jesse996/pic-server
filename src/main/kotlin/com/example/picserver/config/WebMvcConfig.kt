00a528538a60fc8fa6697bce7891fa727a489c9c
Jesse <szx961102@gmail.com>
2021-08-09 22:29:13 +0800

    add webmvcconfig

---
+++ src/main/kotlin/com/example/picserver/config/WebMvcConfig.kt
@@ -0,0 +1,20 @@
+package com.example.picserver.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    /**
+     * Add handlers to serve static resources such as images, js, and, css
+     * files from specific locations under web application root, the classpath,
+     * and others.
+     * @see ResourceHandlerRegistry
+     */
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("file:C:/Users/jesse/Desktop/pic/spider/images/")
+        super.addResourceHandlers(registry)
+
+    }
+}
