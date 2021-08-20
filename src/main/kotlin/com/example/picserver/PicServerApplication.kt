7f23238c539e347e838f2a705f6e788abe7b92ad
jesse <szx961102@gmail.com>
2021-08-20 15:17:50 +0800

    发送邮箱改为异步

--- src/main/kotlin/com/example/picserver/PicServerApplication.kt
+++ src/main/kotlin/com/example/picserver/PicServerApplication.kt
@@ -3,8 +3,10 @@ package com.example.picserver
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 @MapperScan("com.example.picserver.mapper")
 class PicServerApplication
 
--- src/main/kotlin/com/example/picserver/service/impl/MailServiceImpl.kt
+++ src/main/kotlin/com/example/picserver/service/impl/MailServiceImpl.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
@@ -49,6 +50,7 @@ class MailServiceImpl(val javaMailSender: JavaMailSender,val templateEngine: Tem
         }
     }
 
+    @Async
     override fun sendEnableMail(to: String, id: String) {
         //创建邮件正文
         val context = Context()
