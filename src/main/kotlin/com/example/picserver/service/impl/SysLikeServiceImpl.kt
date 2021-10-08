9653187cd7b3f82f01d080a6819a5c0f81fe5f14
jesse <szx961102@gmail.com>
2021-10-08 17:49:37 +0800

    update

--- src/main/kotlin/com/example/picserver/service/impl/SysLikeServiceImpl.kt
+++ src/main/kotlin/com/example/picserver/service/impl/SysLikeServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.example.picserver.service.impl;
 
+import cn.dev33.satoken.stp.StpUtil
 import com.example.picserver.entity.SysLike;
 import com.example.picserver.mapper.SysLikeMapper;
 import com.example.picserver.service.SysLikeService;
@@ -8,6 +9,7 @@ import com.example.picserver.const.LikeEnum
 import com.example.picserver.entity.vo.LikeReq
 import com.example.picserver.service.UserService
 import org.springframework.stereotype.Service;
+import kotlin.text.Typography.tm
 
 /**
  * <p>
@@ -20,21 +22,24 @@ import org.springframework.stereotype.Service;
 @Service
 open class SysLikeServiceImpl(val userService: UserService) : ServiceImpl<SysLikeMapper, SysLike>(), SysLikeService {
     override fun like(likeReq: LikeReq): Boolean {
-        TODO("get uset id")
+        val userId = StpUtil.getLoginIdAsLong()
+
         val like = this.ktQuery()
             .eq(SysLike::type, likeReq.type)
+            .eq(SysLike::userId, userId)
             .eq(SysLike::objId, likeReq.objId)
             .one()
         if (like == null) {
             val tmp = SysLike()
             tmp.type = LikeEnum.PIC.code
             tmp.objId = likeReq.objId
+            tmp.userId = userId
             return this.save(tmp)
         }
         return false
     }
 
-    override fun getLikeCount(likeReq: LikeReq):Int =
+    override fun getLikeCount(likeReq: LikeReq): Int =
         this.ktQuery()
             .eq(SysLike::type, likeReq.type)
             .eq(SysLike::objId, likeReq.objId)
